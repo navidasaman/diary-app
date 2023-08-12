5b220bb6770907f1e34d826c1a1047a7c159ba65
navidasaman <navidasaman@gmail.com>
2023-08-12 16:27:08 +0200

    Added comments to the Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,13 +3,17 @@ import '../css/Sidebar.css'
 import { FaHome, FaBookDead, FaComments, FaRegCalendarAlt, FaLaughBeam, FaPalette, FaUserAlt, FaTimes, FaBars, FaMailBulk } from 'react-icons/fa';
 
 function Sidebar() {
+  /*The state variable is set to false as the initial value*/ 
   const [isCollapsed, setIsCollapsed] = useState(false);
 
+  /*To be able to toggle the sidebar between true and false to collapse/expand it */
   const handleToggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
     console.log(isCollapsed);
   };
 
+  /* the useEffect hook is used alongside an event listener to automatically collapse 
+  the sidebar if window size is under 900px by changing the state of the useState hook */
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 900) {
@@ -19,14 +23,16 @@ function Sidebar() {
       }
     };
 
+    /* Event listener for the resize event */
     window.addEventListener('resize', handleResize);
 
-    // Cleanup the event listener on component unmount
+    /*Removing the event listener as it unmounts */
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
+  /*Creates objects in an array for the items in the sidebar representing the title, icon and route link*/
   const SidebarData = [
         {
             title: "Home",
@@ -65,22 +71,24 @@ function Sidebar() {
       }
   ];
 
+  /*Returns the sidebar containing the object from the SidebarData array and 
+  the toggle button alongside conditions for when the sidebar is collapsed or not */
   return (
-<div className={`Sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-    <button className="ToggleButton" onClick={handleToggleSidebar}>{isCollapsed ? <FaBars /> : <FaTimes />}</button>
-    <div className='MenuContainer'>
-      <div className={`MenuTitle ${isCollapsed ? 'collapsed' : ''}`}><FaUserAlt /></div>
+    <div className={`Sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+      <button className="ToggleButton" onClick={handleToggleSidebar}>{isCollapsed ? <FaBars /> : <FaTimes />}</button>
+      <div className='MenuContainer'>
+        <div className={`MenuTitle ${isCollapsed ? 'collapsed' : ''}`}><FaUserAlt /></div>
+      </div>
+      <div className="SidebarList">
+        {SidebarData.map((val, key) => (
+          <div key={key} className="SidebarRow" onClick={() => (window.location.pathname = val.link)}>
+            <div className="SidebarIcon">{val.icon}</div> 
+            <div className={`SidebarTitle ${isCollapsed ? 'collapsed' : ''}`}>{val.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  
-    <div className="SidebarList">
-      {SidebarData.map((val, key) => (
-        <div key={key} className="SidebarRow" onClick={() => (window.location.pathname = val.link)}>
-          <div className="SidebarIcon">{val.icon}</div> 
-          <div className={`SidebarTitle ${isCollapsed ? 'collapsed' : ''}`}>{val.title}</div>
-        </div>
-      ))}
-    </div>
-  </div>
   );
 }
+
 export default Sidebar
