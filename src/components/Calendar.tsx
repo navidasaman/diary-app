1250e453edfee58840ba6ddd630c90c8d6d08dbc
navidasaman <navidasaman@gmail.com>
2023-08-15 19:41:00 +0200

    calendar UI

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,24 +1,33 @@
-import '../css/Calendar.css'
-import Fullcalendar  from '@fullcalendar/react';
+import React, { useState } from 'react';
+import '../css/Calendar.css';
+import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { EventInput } from '@fullcalendar/core';
 
 function Calendar() {
+  
   return (
-    <div className="calendar">
-      <Fullcalendar 
-        plugins ={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
-        initialView={"dayGridMonth"}
+    <div className='calendar'>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable={true} // will let the user select time and date
+        editable={true} // will let user drag and resize events
+        dayMaxEvents={true} // limits number of max events shown in one day
+        navLinks={true} // will enable switching between the views day/week/month
+        // shows what the header will contain
         headerToolbar={{
-          start: 'dayGridMonth, timeGridWeek, timeGridDay',
+          start: 'dayGridMonth, timeGridWeek, timeGridDay', 
           center: 'title',
           end: 'today prev,next', 
         }}
-        height={'90vh'}       
+        height={'84vh'}
       />
     </div>
-  )
+
+  );
 }
 
-export default Calendar
+export default Calendar;
