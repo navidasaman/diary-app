219a09b3471c5f36a8ede2a07abc5d3ea2f217e5
navidasaman <navidasaman@gmail.com>
2023-08-15 20:24:12 +0200

    created a popup box which shows up as a slot is clicked on the calendar
    popup allowing user to fill in title, description and date alongside cancel action

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -7,8 +7,34 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { EventInput } from '@fullcalendar/core';
 
 function Calendar() {
-  
-  return (
+  // State variable to hold events
+  const [events, setEvents] = useState<EventInput[]>([]); 
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupTitle, setPopupTitle] = useState('');
+  const [popupDescription, setPopupDescription] = useState('');
+  const [popupStartDate, setPopupStartDate] = useState('');
+  const [popupEndDate, setPopupEndDate] = useState('');
+
+  // To select date in calendar which is extracted from the arg and stored into the state variabales
+  const handleDateSelect = (arg: any) => {
+    setPopupStartDate(arg.startStr); 
+    setPopupEndDate(arg.endStr);
+    setShowPopup(true);
+  };
+
+  // function handleEventClick which fires as a slot in the calendar is pressed
+  const handleEventClick = (arg: any) => {
+    const event = arg.event; // the click event object is extracted from the arg object which contains the properties in the line below
+    const { id, title, start, end, extendedProps } = event; // extracts the following properties from the event object
+    // state setter functions to set the state of the variables
+    setPopupTitle(title);
+    setPopupStartDate(start.toISOString());
+    setPopupEndDate(end.toISOString());
+    setPopupDescription(extendedProps.description || '');
+    setShowPopup(true);
+  };
+
+ return (
     <div className='calendar'>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -17,6 +43,14 @@ function Calendar() {
         editable={true} // will let user drag and resize events
         dayMaxEvents={true} // limits number of max events shown in one day
         navLinks={true} // will enable switching between the views day/week/month
+        events={events}
+        select={handleDateSelect} 
+        eventClick={handleEventClick}
+        eventContent={(arg) => (
+          <div>
+            <div>{arg.event.title}</div>
+          </div>
+        )}
         // shows what the header will contain
         headerToolbar={{
           start: 'dayGridMonth, timeGridWeek, timeGridDay', 
@@ -25,6 +59,40 @@ function Calendar() {
         }}
         height={'84vh'}
       />
+      
+      {/* Showcases the popup with its properties */}
+      {showPopup && (
+        <div className="popup">
+          <h1>Information.</h1>
+          <div>
+            <input
+              type="text"
+              value={popupTitle}
+              onChange={(e) => setPopupTitle(e.target.value)}
+              placeholder="Event name"
+            />
+            <textarea
+              style={{ height: '80px' }}
+              value={popupDescription}
+              onChange={(e) => setPopupDescription(e.target.value)}
+              placeholder="Description"
+            />
+            <label>Start Date:</label>
+            <input
+              type="datetime-local"
+              value={popupStartDate}
+              onChange={(e) => setPopupStartDate(e.target.value)}
+            />
+            <label>End Date:</label>
+            <input
+              type="datetime-local"
+              value={popupEndDate}
+              onChange={(e) => setPopupEndDate(e.target.value)}
+            />
+            <button onClick={() => setShowPopup(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
 
   );
