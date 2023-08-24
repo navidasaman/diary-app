679c29b4708ebd910444c9300d41a52fb5858b05
navidasaman <navidasaman@gmail.com>
2023-08-24 09:41:39 +0200

    Added a title input to diary entries

--- src/components/DiaryEntry.tsx
+++ src/components/DiaryEntry.tsx
@@ -4,6 +4,7 @@ import '../css/Diary.css'
 // Gives the wanted structure to the entry object where date and the content of the diary entry will be visible
 interface Entry {
     date: string;
+    title: string;
     content: string;
   }
 
@@ -11,22 +12,26 @@ function DiaryEntry() {
   // State management variables which will be modified and updated
   const [value, setValue] = useState('');
   const [entries, setEntries] = useState<Entry[]>([]);
+  const [title, setTitle] = useState('');
 
   // Creates the diary entry and adds the entries to array
   const handleSubmit = () => {
     const newEntry: Entry = {
         date: new Date().toLocaleString(),
+        title: title,
         content: value
       };
   
     setEntries(previousEntries => [...previousEntries, newEntry]);
     setValue('');
+    setTitle('');
   }
   
   // 
   const DiaryEntryComponent: React.FC<{ entry: Entry }> = ({ entry }) => (
     <div>
       <div className='entryDate'>{entry.date}</div>
+      <div className='entryTitle'>• {entry.title} •</div>
       <div className='entryText'>{entry.content}</div>
       <hr />
     </div>
@@ -35,6 +40,12 @@ function DiaryEntry() {
   return (
     <div className='textEditor'>
         <h1 className="diaryDesc">Diary.</h1>
+        <input type="text" 
+            className='diaryTitleInput'
+            placeholder='Diary title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}>
+        </input>
         <textarea 
             className="writingSpace" 
             placeholder='Write your thoughts here'
