acfc01d64f8f5ba68b49eb0b13862730bcc636f8
navidasaman <navidasaman@gmail.com>
2023-08-28 18:44:17 +0200

    Added MySQL database connection to store diary posts and changed code in diaryentry component
    integrated express, mysql, cors

--- src/components/DiaryEntry.tsx
+++ src/components/DiaryEntry.tsx
@@ -1,68 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Diary.css'
+import axios from 'axios'; // To be able to make HTTP requests
 
-// Gives the wanted structure to the entry object where date and the content of the diary entry will be visible
-interface Entry {
-    date: string;
-    title: string;
-    content: string;
-  }
-
-function DiaryEntry() {
-  // State management variables which will be modified and updated
-  const [value, setValue] = useState('');
-  const [entries, setEntries] = useState<Entry[]>([]);
+export default function DiaryEntry() {
+  // State management variables with empty strings which will be modified and updated
   const [title, setTitle] = useState('');
+  const [post, setPost] = useState('');
+  const [date, setDate] =  useState('');
+  
+  // Sets the date and time of submission
+  useEffect(() =>{
+    setDate(new Date().toLocaleString());
+  }, []);
 
-  // Creates the diary entry and adds the entries to array
   const handleSubmit = () => {
-    const newEntry: Entry = {
-        date: new Date().toLocaleString(),
-        title: title,
-        content: value
-      };
-  
-    setEntries(previousEntries => [...previousEntries, newEntry]);
-    setValue('');
-    setTitle('');
+     // Sends a post request to the specified URL
+    axios.post('http://localhost:3001/api/create', 
+    { // request body:
+      title: title,
+      post: post,
+      date: date
+    }) // Promise which only executes after the post request has been successfully made
+      .then(() => {
+        // Reset form fields after successful submission
+        setTitle('');
+        setPost('');
+        setDate(new Date().toLocaleString());
+      })
+      .catch((error) => {
+        console.error('Error submitting data:', error);
+      });
   }
-  
-  // 
-  const DiaryEntryComponent: React.FC<{ entry: Entry }> = ({ entry }) => (
-    <div>
-      <div className='entryDate'>{entry.date}</div>
-      <div className='entryTitle'>• {entry.title} •</div>
-      <div className='entryText'>{entry.content}</div>
-      <hr />
-    </div>
-  );
 
   return (
-    <div className='textEditor'>
-        <h1 className="diaryDesc">Diary.</h1>
-        <input type="text" 
-            className='diaryTitleInput'
-            placeholder='Diary title'
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}>
-        </input>
-        <textarea 
-            className="writingSpace" 
-            placeholder='Write your thoughts here'
-            value={value}
-            onChange={(e) => setValue(e.target.value)}>
-        </textarea>
-        <button className='submitButton' onClick={handleSubmit}>Submit</button>
-        
-        <div className='entriesContainer'>
-            {entries.map((entry, index) => (
-                <DiaryEntryComponent key={index} entry={entry} />
-            ))}
-        </div>
-    </div>
-    
+    <form onSubmit={handleSubmit}>
+      <div className='textEditor'>
+          <h1 className="diaryDesc">Diary.</h1>
+          <input type="text" 
+              className='diaryTitleInput'
+              placeholder='Diary title'
+              onChange={(e) => setTitle(e.target.value)}>
+          </input>
+          <textarea 
+              className="writingSpace" 
+              placeholder='Write your thoughts here'
+              onChange={(e) => setPost(e.target.value)}>
+          </textarea>
+          <button className='submitButton' type="submit">Submit</button>   
+      </div>
+    </form>
 );
-
 }
-
-export default DiaryEntry;
